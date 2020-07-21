0188f678b26f3d4d2c1a819332475b5c1c8f7b3f
mramos89 <moises.rams7@gmail.com>
2020-07-20 19:34:08 -0600

    commit 2 mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -15,8 +15,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    //urlDB = 'mongodb://localhost:27017/cafe'
-    urlDB = 'mongodb+srv://mramos89:71o29seXDQDTvPWx@cluster0.d7evh.mongodb.net/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe'
+        //urlDB = 'mongodb+srv://mramos89:71o29seXDQDTvPWx@cluster0.d7evh.mongodb.net/cafe'
 
 } else {
     urlDB = 'mongodb+srv://mramos89:71o29seXDQDTvPWx@cluster0.d7evh.mongodb.net/cafe'
